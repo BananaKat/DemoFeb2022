4fe7f3c70001d0c155b063748f5393fd1d5dde4e
Jason Phua <438973311@student.sbhs.nsw.edu.au>
2022-03-01 12:22:53 +1100

    Adding experiment

---
+++ experiment.py
@@ -0,0 +1,6 @@
+def calc_quote(numAdults, numChildren, cleaningRequired):
+    print('hello')
+
+numAdults = int(input('Enter the number of adults: '))
+numChildren = int(input('Enter the number of adults: '))
+calc_quote(12,4,Fakse)
